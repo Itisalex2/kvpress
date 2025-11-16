83838f6e59510f9156c9ece4ee74d1d1e55d3b8b
Alex Chen <chenalexlu@gmail.com>
2025-11-15 16:55:12 -0800

    fix: add streamingllm interpolate press to list of presses

--- kvpress/__init__.py
+++ kvpress/__init__.py
@@ -47,6 +47,7 @@ from kvpress.presses.streaming_llm_system_prompt_press import (
 from kvpress.presses.streamingllm_fair_eviction_press import (
     StreamingLLMFairEvictionPress,
 )
+from kvpress.presses.streaming_llm_interpolate_press import StreamingLLMInterpolatePress
 from kvpress.presses.expected_eviction_press import ExpectedEvictionPress
 
 # Patch the attention functions to support head-wise compression
@@ -88,6 +89,7 @@ __all__ = [
     "SnapKVFairEvictionPress",
     "StreamingLLMSystemPromptPress",
     "StreamingLLMFairEvictionPress",
+    "StreamingLLMInterpolatePress",
     "KnormFairEvictionPress",
     "TOVAFairEvictionPress",
     "ExpectedEvictionPress",
